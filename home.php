b46512f0ffc89af3af6fe68a0f0e6b90af1dcaf5
Poporn16 <kerviniruguin96@gmail.com>
2025-01-12 15:31:59 +0800

    php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script text="text/javascript" src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+    <title>CvSU - GenTri Campus</title>
+</head>
+<body>
+    <section class="header">
+        <div class="logo container-fluid bg-secondary">
+            <a href="home.html"><img src="fastfood-logo.png"></a>
+                <li><a href="">HOME</a></li>
+                <li><a href="">RESERVATION</a></li>
+                <li><a href="">HOME</a></li>
+                <li><a href="">CONTACT US</a></li>
+                <li><a href="">ABOUT US</a></li>
+        </div>
+    </section>
+</body>
+</html>
