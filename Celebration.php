879a11e96e0ff084489dac9416a062944fe8c37f
frnzn31 <fabrerof@gmail.com>
2025-01-13 01:59:56 +0800

    Changes

--- Celebration.php
+++ Celebration.php
@@ -6,6 +6,7 @@
     <script text="text/javascript" src="js/bootstrap.min.js"></script>  
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="Celebration.css">
     <title>Celebration</title>
 </head>
 <body>
@@ -19,11 +20,38 @@
         </nav>
     </header>
     <br><br><br><br><br><br><br>
-    <p class="text-success text-center fs-1"> Book your Celebration!</p>
+    <p class="text-success text-center" style="font-size: 50px;"><i> Book &nbsp&nbsp <span style="font-size: 35px;">your</span> <strong> &nbsp&nbsp Celebration!</strong</i></p>
+   
     <div class="container">
-
+        <div class="text">
+            <i style="font-size: 40px; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Have a Special Occasion?!</i>
+            <p style="font-size: medium;">
+                Ready for a Surprise?!
+                The hottest take in celebrating a special day is coming to town, Readily served by your beloved Mang Inasal Fast Food Chain
+            </p>
+        </div>
+        <div class="image">
+            <img src="Celebrate.jpg" alt="Celebration">
+        </div>
     </div>
-    
+
+    <div class="container">
+        <div class="image1">
+            <img src="logo.png" alt="Description of the image">
+        </div>
+        <div class="text1">
+            <i style="font-size: 40px; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Did you see it Coming!!</i>
+            <p style="font-size: medium;">
+              One of your favorite fast food chains in the philippines is now offering the one of the hottest 
+              and gri-lliest Special Celebrations in the phillipines, Introducing the <span style="text-decoration: underline;"> Mang-Party Celebration </span>
+            </p>
+        </div>
+    </div>
+
+
+    <div class="container mt-5">
+        <a href="" class="btn1 btn-success btn-lg text-dark" role="button"> <strong style="font-size:35px"> Book Now!</strong> </a>
+    </div>
     
 </body>
 </html>
