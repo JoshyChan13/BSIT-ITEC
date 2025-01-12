52c439883b5b237411006b348ab84ba9b80eb2e2
Poporn16 <96321127+Poporn16@users.noreply.github.com>
2025-01-12 16:03:31 +0800

    Update home.php

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
     <title>CvSU - GenTri Campus</title>
 </head>
 <body>
-    <section class="header">
+    <nav>
         <div class="logo container-fluid bg-secondary">
             <a href="home.html"><img src="fastfood-logo.png"></a>
                 <li><a href="">HOME</a></li>
@@ -18,6 +18,6 @@
                 <li><a href="">CONTACT US</a></li>
                 <li><a href="">ABOUT US</a></li>
         </div>
-    </section>
+    <nav>
 </body>
 </html>
