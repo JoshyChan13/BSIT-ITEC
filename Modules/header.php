ca327c756b2a725a8eb7dcda4cd7d04c247cda64
Poporn16 <kerviniruguin96@gmail.com>
2025-01-20 23:04:47 +0800

    Update header.php

--- Modules/header.php
+++ Modules/header.php
@@ -22,7 +22,7 @@ if(!empty($_SESSION["Username"])){
         <div class="logo"><a href=""><img src="img/logo.png" style="width: 110px;"></a></div>
         <nav>
             <ul>
-                <li><!-- From Uiverse.io by andrei0x309 --> 
+                <li>
                 <div class="input">
                     <button class="value" onclick="location.href='home.php'">
                         <svg class="normal" viewBox="0 0 24 24">
@@ -76,7 +76,7 @@ if(!empty($_SESSION["Username"])){
                         <path transform="translate(-3.62 -0.85)" d="M28,27.3,26.24,7.51a.75.75,0,0,0-.76-.69h-3.7a6,6,0,0,0-12,0H6.13a.76.76,0,0,0-.76.69L3.62,27.3v.07a4.29,4.29,0,0,0,4.52,4H23.48a4.29,4.29,0,0,0,4.52-4ZM15.81,2.37a4.47,4.47,0,0,1,4.46,4.45H11.35a4.47,4.47,0,0,1,4.46-4.45Zm7.67,27.48H8.13a2.79,2.79,0,0,1-3-2.45L6.83,8.34h3V11a.76.76,0,0,0,1.52,0V8.34h8.92V11a.76.76,0,0,0,1.52,0V8.34h3L26.48,27.4a2.79,2.79,0,0,1-3,2.44Zm0,0"></path>
                     </svg>
                 </button></li>
-                <li><!-- From Uiverse.io by JaydipPrajapati1910 --> 
+                <li>
                     <button class="Btn" id="logoutButton">
                         <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
                         <div class="text">Logout</div>
